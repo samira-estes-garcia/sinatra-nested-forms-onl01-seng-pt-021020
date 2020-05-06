59c53f165bcccb020307268278af63cc41f37593
Samira Estes-Garcia <samira.estes.garcia@gmail.com>
2020-05-06 22:01:30 +0000

    Automatically backed up by Learn

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,17 @@
 class Pirate
+  
+  attr_accessor :name, :weight, :height
+  PIRATES = []
+  
+  def initialize(name, weight, height)
+    @name = args[:name] 
+    @weight = args[:weight]
+    @height = args[:height]
+    PIRATES << self
+  end
+  
+  def self.all 
+    PIRATES 
+  end
+
 end

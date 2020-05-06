0a1395515fa239297ad93249a16773f02fca4bae
Samira Estes-Garcia <samira.estes.garcia@gmail.com>
2020-05-06 22:22:01 +0000

    Automatically backed up by Learn

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -3,7 +3,7 @@ class Pirate
   attr_accessor :name, :weight, :height
   PIRATES = []
   
-  def initialize(name, weight, height)
+  def initialize(args)
     @name = args[:name] 
     @weight = args[:weight]
     @height = args[:height]
